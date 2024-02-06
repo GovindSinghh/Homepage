5715655a76418f33f71eb3435763ca8b5cd74c31
GovindSinghh <143206234+GovindSinghh@users.noreply.github.com>
2024-02-06 16:27:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar.js";
+import Main from "./components/main.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default App;
